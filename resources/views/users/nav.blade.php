79f8b5897099783e5350efbc5a2431883ba3391b
milandarai90 <milandarai90@gmail.com>
2024-04-25 16:45:12 +0545

    ahsjad

---
+++ resources/views/users/nav.blade.php
@@ -0,0 +1,22 @@
+
+<nav class="navbar navbar-expand-lg bg-secondary p-3">
+
+<DIV class="col-3 d-flex justify-content-center "><a class="navbar-brand text-light fs-5 fw-bold" href="/home">NEOD</a></DIV>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+  <span class="navbar-toggler-icon"></span>
+</button>
+
+<div class="collapse navbar-collapse col-7 d-flex justify-content-center" id="navbarSupportedContent">
+  <ul class="navbar-nav mr-auto">
+    <a href="" class="text-decoration-none text-light"><li>Home</li></a>
+  </ul>                 
+</div>
+<div class="col-2 d-flex justify-content-center">
+
+    <a href="{{route('loginDisplay')}}" class="d-flex justify-conteny-end text-decoration-none"><li class="btn btn-success me-2 border border-3 border-success fw-bold">Login</li></a>
+    <a href="{{route('registerDisplay')}}" class="d-flex justify-conteny-end text-decoration-none"><li class="btn btn-primary border border-3 border-primary fw-bold">Register</li></a>
+</div>
+</nav>
+
+
+ 
